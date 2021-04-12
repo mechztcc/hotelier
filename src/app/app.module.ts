be8153db7ca8c2d68cd1f84017178a225398c714
mechztcc <dojathan@gmail.com>
2021-04-12 11:40:40 -0300

    Creting home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NavbarComponent } from './components/template/navbar/navbar.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { CreateAccountComponent } from './components/pages/create-account/create-account.component';
+import { BannerComponent } from './components/template/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    CreateAccountComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,

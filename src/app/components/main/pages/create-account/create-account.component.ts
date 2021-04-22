6575a940072b96e4154993c4daec11df0d0fbb10
mechztcc <dojathan@gmail.com>
2021-04-22 14:58:37 -0300

    added form controls alert do create account

--- src/app/components/main/pages/create-account/create-account.component.ts
+++ src/app/components/main/pages/create-account/create-account.component.ts
@@ -2,6 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 
+
+
+type FormErros = {
+  email: string[] | any,
+  password: string[] | any,
+  phone: string[] | any
+}
+
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
@@ -11,15 +19,29 @@ export class CreateAccountComponent implements OnInit {
 
   public form: FormGroup
 
+  public flag: boolean = true;
+
+
+ 
   constructor(private titlePage: Title, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.titlePage.setTitle('Create account')
-
+    
+    
     this.form = this.fb.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-      phone: ['', Validators.required]
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.email
+      ])],
+      password: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(5)
+      ])],
+      phone: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(9)
+      ])]
     })
   }
 
